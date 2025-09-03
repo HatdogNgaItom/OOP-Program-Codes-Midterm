0dde5fda21de5e8baadfbe3f464d3d981b272c16
ACT-STUDENT <ACT-STUDENT@LAB3-PC29>
2025-09-03 14:47:58 +0800

    Moved to github

---
+++ Yagonia_EXE06SimpleInheritanceWithProperties/Yagonia_EXE06SimpleInheritanceWithProperties/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagonia_EXE06SimpleInheritanceWithProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Jack Anthony Dolino Yagonia 
+            //IT306
+            //EXER 06: Simple Inheritance
+            //Create an instance of the Regular Student class
+            RegularStudent regularStudent = new RegularStudent
+            {
+                Name = "Sky Loon",
+                Program = "Bachelors of Informations Technology",
+                Section = "IT306",
+            };
+            //Method to display regular student info
+            regularStudent.BasicInfo();
+            regularStudent.SectionEnrolled();
+
+            //Create an instance of the Irregular Student class
+            IrregularStudent irregularStudent = new IrregularStudent
+            {
+                Name = "Louis Aian Cuyos",
+                Program = "Bachelors of Informations Technology",
+                EnrolledSemUnits = "20 units",
+            };
+            irregularStudent.SemUnitsEnrolled(); //Method to display irregular student info
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Yagonia_EXE06SimpleInheritanceWithProperties/Yagonia_EXE06SimpleInheritanceWithProperties/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagonia_EXE06SimpleInheritanceWithProperties
+{
+    internal class Student
+    {
+        //Properties of the base class
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        //Method to display basic info
+        public void BasicInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine("\n\n\n\n\n\t\t\t\t\t-----------Student Information-----------");
+            Console.WriteLine($"\n\t\t\t\t\tName: {Name}, \n\t\t\t\t\tProgram: {Program}");
+        }
+    }
+
+    class RegularStudent : Student //Derived Class
+    {
+        //Properties of the derived class
+        public string Section { get; set; }
+        //Method to display regular student info
+        public void SectionEnrolled()
+        {
+            Console.WriteLine($"\t\t\t\t\tSection: {Section}");
+
+        }
+    }
+    //Properities of the derived class
+
+    //Method to display regular student info
+    class IrregularStudent : Student //Derived Class
+    {
+        //Properities of the derived class
+        public string EnrolledSemUnits { get; set; }
+        //Method to display irregular student info 
+        public void SemUnitsEnrolled()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine("\n\n\n\n\n\t\t\t\t\t-----------Student Information-----------");
+            Console.WriteLine($"\n\t\t\t\t\tName: {Name}, \n\t\t\t\t\tProgram: {Program}, \n\t\t\t\t\tEnrolled Sem Units: {EnrolledSemUnits}");
+        }
+    }
+}
