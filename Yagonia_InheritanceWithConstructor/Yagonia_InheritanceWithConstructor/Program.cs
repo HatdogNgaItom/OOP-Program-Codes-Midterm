656d7a72efc49f0774cedcc93237362f93e3b12c
ACT-STUDENT <ACT-STUDENT@LAB3-PC29>
2025-09-05 14:53:03 +0800

    moved to github

---
+++ Yagonia_InheritanceWithConstructor/Yagonia_InheritanceWithConstructor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagonia_InheritanceWithConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Jack Anthony Dolino Yagonia 
+            //IT306
+            //EXER 06: Simple Inheritance
+            //Create an instance of the Regular Student class
+            RegularStudent regularStudent = new RegularStudent("Sky Loon", "Bachelors of Informations Technology")
+            {
+                Name = "Sky Loon",
+                Program = "Bachelors of Informations Technology",
+                Section = "IT306",
+            };
+            //Method to display regular student info
+            regularStudent.BasicInfo();
+            regularStudent.SectionEnrolled();
+
+            //Create an instance of the Irregular Student class
+            IrregularStudent irregularStudent = new IrregularStudent("Louis Aian Cuyos", "Bachelors of Informations Technology")
+            {
+                Name = "Louis Aian Cuyos",
+                Program = "Bachelors of Informations Technology",
+                EnrolledSemUnits = "20 units",
+            };
+            irregularStudent.SemUnitsEnrolled(); //Method to display irregular student info
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Yagonia_InheritanceWithConstructor/Yagonia_InheritanceWithConstructor/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagonia_InheritanceWithConstructor
+{
+    internal class Student
+    {
+        //Instantiation of properties of the base class
+        public string Name;
+        public string Program;
+        public Student(string StudentFullName, string StudentProgram) //Constructor of the base class
+        {
+            Name = StudentFullName;
+            Program = StudentProgram;
+        }
+      
+        //Method to display basic info
+        public void BasicInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine("\n\n\n\n\n\t\t\t\t\t-----------Student Information-----------");
+            Console.WriteLine($"\n\t\t\t\t\tName: {Name}, \n\t\t\t\t\tProgram: {Program}");
+        }
+    }
+
+    class RegularStudent : Student //Derived Class
+    {
+    
+        //Properties of the derived class
+        public string Section;
+        public RegularStudent(string StudentFullName, string StudentProgram) : base(StudentFullName, StudentProgram) //Constructor of the derived class
+        {
+            Name = StudentFullName;
+            Program = StudentProgram;
+        }
+        //Method to display regular student info
+        public void SectionEnrolled()
+        {
+            Console.WriteLine($"\t\t\t\t\tSection: {Section}");
+
+        }
+    }
+    //Properities of the derived class
+
+    //Method to display regular student info
+    class IrregularStudent : Student //Derived Class
+    {
+        //Properities of the derived class
+        public string EnrolledSemUnits;
+        public IrregularStudent(string StudentFullName, string StudentProgram) : base(StudentFullName, StudentProgram) //Constructor of the derived class
+        {
+            Name = StudentFullName;
+            Program = StudentProgram;
+        }
+        //Method to display irregular student info 
+        public void SemUnitsEnrolled()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine("\n\n\n\n\n\t\t\t\t\t-----------Student Information-----------");
+            Console.WriteLine($"\n\t\t\t\t\tName: {Name}, \n\t\t\t\t\tProgram: {Program}, \n\t\t\t\t\tEnrolled Sem Units: {EnrolledSemUnits}");
+        }
+    }
+}
