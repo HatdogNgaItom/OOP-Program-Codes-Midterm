8c674d34829ee8c9c066593ec9b0c9f8fb0125d9
ACT-STUDENT <ACT-STUDENT@LAB3-PC29>
2025-09-08 14:44:51 +0800

    moved to folder

---
+++ Yagonia_EXER08/Yagonia_EXER08/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagonia_EXER08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jack Anthony D. Yagonia
+            //IT306
+            //EXER_08
+
+            //Instantiate Shoe class
+            Shoes myShoes = new Shoes();
+
+            //Set the properties of the Shoe class
+            myShoes.brand = "Nike";
+            myShoes.model = "Nike P6000";
+
+            //Display the properties of the Shoe class
+            myShoes.
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Yagonia_EXER08/Yagonia_EXER08/Shoes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagonia_EXER08
+{
+    internal class Shoes
+    {
+        public string brand;
+        public string model;
+
+        public Shoes(string abrand, string amodel )
+        {
+            brand = abrand;
+            model = amodel;
+            
+
+        }
+
+        //Method to display shoe details
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Shoe Brand: " + brand);
+            Console.WriteLine("Shoe Model: " + model);
+        }
+    }
+}
