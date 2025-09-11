84eeb31c0efb6ddb4461d64b2ea61670b4507a3b
Jack Anthony Dolino Yagonia <jackyagonia9@gmail.com>
2025-09-11 16:59:01 +0800

    Update Shoes.cs
    Added 2 derived classes(TypeofFoam & ColorofShoe)

--- Yagonia_EXER08/Yagonia_EXER08/Shoes.cs
+++ Yagonia_EXER08/Yagonia_EXER08/Shoes.cs
@@ -26,4 +26,33 @@ namespace Yagonia_EXER08
             Console.WriteLine("Shoe Model: " + model);
         }
     }
+    class TypeofFoam: Shoes
+    {
+       //Fields
+       public string FoamType
+       //Constructor
+       public TypeofFoam(string abrand, string amodel, string afoamtype ) : base(abrand, amodel)
+       {
+           FoamType = aOutsoleFoamType;
+       }
+       public void RunningDetails()
+       {
+           Console.WriteLine($"What type of outsole foam do you want: ");
+       }
+    }
+    class ColorofShoe
+    {
+        //Fields
+        public string ColorofShoe
+        //Constructor
+        public ColorofShoe(string abrand, string amodel, string acolorofshoe)
+        {
+            ColorofShoe = acolorofshoe;
+        }
+        //Method
+        public void ColorofShoeDetails()
+        {
+            Console.WriteLine($"What color of shoe do you prefer:");
+        }
+    }
 }
