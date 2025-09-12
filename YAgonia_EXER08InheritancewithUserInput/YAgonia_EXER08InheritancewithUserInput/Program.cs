8871e6c10fd2fa6d5c622cef854fa04be2ef4e24
ACT-STUDENT <ACT-STUDENT@LAB3-PC29>
2025-09-12 14:24:58 +0800

    Moved to github

---
+++ YAgonia_EXER08InheritancewithUserInput/YAgonia_EXER08InheritancewithUserInput/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static YAgonia_EXER08InheritancewithUserInput.Shoes;
+
+namespace YAgonia_EXER08InheritancewithUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jack Anthony Dolino Yagonia
+            //IT306
+            //EXER08_Inheritance with User Input
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\t\t\t\t CHOOSE WHAT TYPE OF SHOES ARE YOU LOOKING FOR:");
+            Console.WriteLine("\t\t\t\t\t\t1. Running Shoes");
+            Console.WriteLine("\t\t\t\t\t\t2. Casual Shoes");
+            Console.Write("\t\t\t\t\t Enter your choice (1 or 2): ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\t\t\t\t\t --You chose Running Shoes.--");
+
+                Console.Write("\t\t\t\t\tEnter the brand of the shoe: ");
+                string brand = Console.ReadLine();
+
+                Console.Write("\t\t\t\t\tEnter the model of the shoe: ");
+                string model = Console.ReadLine();
+
+                Console.Write("\t\t\t\t\tEnter the foam type: ");
+                string foam = Console.ReadLine();
+
+                TypeofFoam runningShoe = new TypeofFoam(brand, model, foam);
+                runningShoe.DisplayDetails();
+            }
+            else if (choice == "2")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\t\t\t\t\t --You chose Casual Shoes.--");
+
+                Console.Write("\t\t\t\t\tEnter the brand of the shoe: ");
+                string brand = Console.ReadLine();
+
+                Console.Write("\t\t\t\t\tEnter the model of the shoe: ");
+                string model = Console.ReadLine();
+
+                Console.Write("\t\t\t\t\tEnter the shoe color: ");
+                string color = Console.ReadLine();
+
+                ColorofShoe casualShoe = new ColorofShoe(brand, model, color);
+                casualShoe.DisplayDetails();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\t\t\t\t\tInvalid choice. Please select 1 or 2.");
+            }
+
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ YAgonia_EXER08InheritancewithUserInput/YAgonia_EXER08InheritancewithUserInput/Shoes.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAgonia_EXER08InheritancewithUserInput
+{
+    internal class Shoes
+    {
+        // Base class
+        public class Shoe
+        {
+            private string brand;
+            private string model;
+
+            // Parameterless constructor
+            public Shoe()
+            {
+                brand = "Unknown";
+                model = "Unknown";
+            }
+
+            // Parameterized constructor
+            public Shoe(string brand, string model)
+            {
+                this.brand = brand;
+                this.model = model;
+            }
+
+            // Properties (Encapsulation)
+            public string Brand
+            {
+                get { return brand; }
+                set { brand = value; }
+            }
+
+            public string Model
+            {
+                get { return model; }
+                set { model = value; }
+            }
+
+            // Method
+            public virtual void DisplayDetails()
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n\t\t\t\t---Here are the details for your chosen shoes---");
+                Console.WriteLine($"\t\t\t\t\t\tBrand: {brand},\n\t\t\t\t\t\tModel: {model}");
+                Console.ResetColor();
+            }
+        }
+
+        // Derived class: Running Shoe with Foam Type
+        public class TypeofFoam : Shoe
+        {
+            private string foamType;
+
+            public TypeofFoam(string brand, string model, string foamType) : base(brand, model)
+            {
+                this.foamType = foamType;
+            }
+
+            public override void DisplayDetails()
+            {
+                base.DisplayDetails();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"\t\t\t\t\t\tFoam Type: {foamType}");
+                Console.ResetColor();
+            }
+        }
+
+        // Derived class: Casual Shoe with Color
+        public class ColorofShoe : Shoe
+        {
+            private string color;
+
+            public ColorofShoe(string brand, string model, string color) : base(brand, model)
+            {
+                this.color = color;
+            }
+
+            public override void DisplayDetails()
+            {
+                base.DisplayDetails();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"\t\t\t\t\t\tColor: {color}");
+                Console.ResetColor();
+            }
+        }
+    }
+}
